8d4f3a6f23d35b99650ffde7759d45f6ac09e1fb
alamnahid <nahidalam13765@gmail.com>
2023-11-10 22:09:52 +0600

    implement nmenu

--- src/Pages/Home/Popular Menu/PopularMenu.jsx
+++ src/Pages/Home/Popular Menu/PopularMenu.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../Shared/SectionTitle";
+import MenuItem from "../../Shared/Menu Items/MenuItem";
 
 
 const PopularMenu = () => {
+
+    const [menu, setMenu] = useState([])
+
+    useEffect(()=>{
+        fetch('/menu.json')
+        .then(res=>res.json())
+        .then(data=>{
+            const popularItems = data.filter(item=>item.category === 'popular');
+            setMenu(popularItems)
+        })
+    }, [])
+    console.log(menu)
+
+
     return (
         <div>
 
             <SectionTitle subHeading={'---Check it out---'}
                 heading={'FROM OUR MENU'} ></SectionTitle>
+
+                <div className="grid gap-8 grid-cols-1 md:grid-cols-2">
+                    {
+                        menu?.map(item=><MenuItem
+                        key={item._id}
+                        item={item}
+                        ></MenuItem>)
+                    }
+                </div>
 
         </div>
     );
---
+++ src/Pages/Shared/Menu Items/MenuItem.jsx
@@ -0,0 +1,29 @@
+
+
+const MenuItem = ({ item }) => {
+
+    const { name, image, recipe, price } = item;
+
+
+    return (
+        <div>
+
+            <div className="flex">
+                <img className="w-[7.3rem] h-[6.5rem]" style={{
+                    borderRadius: "0rem 12.5rem 12.5rem 12.5rem"
+                }} src={image} alt="food image" />
+                <div className="ml-8">
+                    <h3 className="text-[#151515] uppercase text-xl .cin">ROAST DUCK BREAST ------------------</h3>
+                    <p className="text-[#737373] w-[27.5rem] text-base mt-2">Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce</p>
+                </div>
+               
+                    <p className="text-[#BB8506] ml-1 text-xl">$14.5</p>
+                
+            </div>
+
+
+        </div>
+    );
+};
+
+export default MenuItem;
