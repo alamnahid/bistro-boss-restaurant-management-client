40b2d28e1afbf155eb266540bc73ef3ba2794f00
alamnahid <nahidalam13765@gmail.com>
2023-11-13 11:10:24 +0600

    implement login page and configure firebase

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,12 +3,46 @@ import logininimage from "../../assets/others/authentication2.png"
 import fb from "../../assets/icon/fb.svg"
 import google from "../../assets/icon/google.svg"
 import github from "../../assets/icon/git.svg"
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { useEffect, useRef } from "react"
+
 
 const Login = () => {
-    return (
-        <div style={{ backgroundImage: `url(${loginbg})` }} className="w-[100vw] py-[10vh] h-[100vh]">
 
-            <div style={{ backgroundImage: `url(${loginbg})`, boxShadow: "10px 10px 10px 10px rgba(0, 0, 0, 0.25)" }} className="max-w-screen-2xl h-[80vh] mx-auto flex justify-around items-center">
+    const captchaRef = useRef(null)
+    // const [capcthavalue, setCaptchaValue] =
+
+    useEffect(()=>{
+        loadCaptchaEnginge(6);
+    }, [])
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        const user_captcha_value  = captchaRef.current.value;
+        if (validateCaptcha(user_captcha_value)==true) {
+            alert('Captcha Matched');
+        }
+   
+        else {
+            alert('Captcha Does Not Match');
+        }
+        
+
+        console.log(email, password, user_captcha_value)
+
+    }
+
+
+
+
+    return (
+        <div style={{ backgroundImage: `url(${loginbg})` }} className="w-[100vw] py-[5vh] h-[100vh]">
+
+            <div style={{ backgroundImage: `url(${loginbg})`, boxShadow: "10px 10px 10px 10px rgba(0, 0, 0, 0.25)" }} className="max-w-screen-2xl h-[90vh] mx-auto flex justify-around items-center">
 
                 {/* image  */}
                 <div>
@@ -18,35 +52,37 @@ const Login = () => {
 
 
                 {/* form  */}
-                <div className="w-[34rem]">
+                <div className="w-[28rem]">
                     <h1 className="text-black text-center text-[2.5rem] font-semibold">Login</h1>
 
-                    <form>
-                        <p className="text-[#444] mt-8 text-xl font-semibold">Email</p>
+                    <form onSubmit={handleSubmit}>
+                        <p className="text-[#444] mt-5 text-xl font-semibold">Email</p>
 
-                        <input className="w-full mt-4 h-[4.5rem] text-gray-700 placeholder:text-[#A1A1A1] text-xl outline-none pl-[1.81rem] rounded-lg border-2 border-[#D0D0D0] bg-white" type="email" name="email" id="" placeholder="Type here" />
+                        <input className="w-full mt-2 h-[3.5rem] text-gray-700 placeholder:text-[#A1A1A1] text-lg outline-none pl-[1.81rem] rounded-lg border-2 border-[#D0D0D0] bg-white" type="email" name="email" id="" placeholder="Type here" />
 
-                        <p className="text-[#444]  text-xl font-semibold">Password</p>
+                        <p className="text-[#444]  text-xl mt-4 font-semibold">Password</p>
 
-                        <input className="w-full mt-4 h-[4.5rem] text-gray-700 placeholder:text-[#A1A1A1] text-xl outline-none pl-[1.81rem] rounded-lg border-2 border-[#D0D0D0] bg-white" type="password" name="password" id="" placeholder="Enter your password" />
+                        <input className="w-full mt-2 h-[3.5rem] text-gray-700 placeholder:text-[#A1A1A1] text-lg outline-none pl-[1.81rem] rounded-lg border-2 border-[#D0D0D0] bg-white" type="password" name="password" id="" placeholder="Enter your password" />
 
 
                        
+                        <div className="flex justify-between items-center gap-3">
 
-                        <input className="w-full mt-10 h-[4.5rem] text-gray-700 placeholder:text-[#A1A1A1] text-xl outline-none pl-[1.81rem] rounded-lg border-2 border-[#D0D0D0] bg-white" type="password" name="password" id="" placeholder="Enter your password" />
+                        <div className="w-full mt-6 bg-white pl-3 h-[3.5rem] rounded-lg border-2 border-[#D0D0D0]">
+                        <LoadCanvasTemplate />
+                        </div>
 
-                        <p className="text-[#5D5FEF] text-xl font-semibold">Reload Captcha</p>
+                        <input className="w-full mt-6 h-[3.5rem] text-gray-700 placeholder:text-[#A1A1A1] text-lg outline-none pl-[1.81rem] rounded-lg border-2 border-[#D0D0D0] bg-white" type="text" name="capctha" ref={captchaRef} placeholder="Type the captcha" />
 
+
+                        </div>
 
                         
+                        <input className="w-full mt-5 h-[3.5rem] btn btn-neutral border-none bg-[#D1A054B3] text-white text-xl font-bold" type="submit"  id="" value="Sign In" />
 
-                        <input className="w-full mt-6 h-[4.5rem] text-gray-700 placeholder:text-[#A1A1A1] text-xl outline-none pl-[1.81rem] rounded-lg border-2 border-[#D0D0D0] bg-white" type="password" name="password" id="" placeholder="Type here" />
+                        <p className="text-[#D1A054] mt-5 text-center text-xl font-medium">New here? <span className="font-bold">Create a New Account</span></p>
 
-                        <input className="w-full mt-8 h-[4.5rem] btn btn-neutral border-none bg-[#D1A054B3] text-white text-xl font-bold" type="submit" name="password" id="" value="Sign In" />
-
-                        <p className="text-[#D1A054] mt-8 text-center text-xl font-medium">New here? <span className="font-bold">Create a New Account</span></p>
-
-                        <p className="text-[#444] mt-6 text-xl font-medium text-center">Or sign in with</p>
+                        <p className="text-[#444] mt-5 text-xl font-medium text-center">Or sign in with</p>
 
                         <div className="flex items-center justify-center gap-14 mt-5">
                             <div className="w-[3.25rem] hover:bg-white cursor-pointer btn-neutral h-[3.25rem] bg-[#F1F2F4] border-2 flex justify-center items-center border-black rounded-[50%] ">
